a3d3e9bedba2dc812250f1ec91973cc3d4aeb744
Colby Klein <shakesoda@gmail.com>
2023-01-31 04:56:28 -0800

    remove more useless I* files. not sure this entire folder is necessary.

--- js/html/ICredential.hx
+++
@@ -1,6 +0,0 @@
-package js.html;
-
-typedef ICredential = {
-	final id : String;
-	final type : String;
-};
--- js/html/IMSMediaKeys.hx
+++
@@ -1,6 +0,0 @@
-package js.html;
-
-typedef IMSMediaKeys = {
-	final keySystem : String;
-	function createSession(type:String, initData:js.lib.Uint8Array, ?cdmData:js.lib.Uint8Array):MSMediaKeySession;
-};
